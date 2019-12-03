2c2bdc5e45d919ea4d45e2443ec5c6f52a381589
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-02 19:19:34 -0500

    update js files with my style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+/// external modules ///
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+
+/// internal modules ///
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+/// styles ///
+// import 'reset-css/reset.css';
+// import 'normalize-css/normalize.css';
+// import './styles/base.css';
+import './styles/index.css';
+
+/***************************************
+  APP
+***************************************/
+ReactDOM.render (
+  <App />
+, document.getElementById ('root')
+);
